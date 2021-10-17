ee317ce7955a58bc0dbb5d696bbbfb18fdbdf0b4
Vishnu <vishnuak15@gmail.com>
2021-10-17 20:44:40 +0530

    first Test

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,17 +1,28 @@
 from django.test import TestCase
 from selenium import webdriver
 
-class FunctionalTestCase(TestCase):# Test class for functional tests
+# class FunctionalTestCase(TestCase):# Test class for functional tests
     
-    def setUp(self):#before all Test are done
-        self.browser = webdriver.Firefox()
+#     def setUp(self):#before all Test are done
+#         self.browser = webdriver.Firefox()
     
-    def test_there_is_homepage(self):
-        self.browser.get('http://127.0.0.1:8000')
-        self.assertIn('install', self.browser.page_source)
+#     def test_there_is_homepage(self):
+#         self.browser.get('http://127.0.0.1:8000')
+#         self.assertIn('Enter hash here:', self.browser.page_source)
 
-        
-    def tearDown(self):#after all Test are done
-        self.browser.quit()
+#     def test_hash_of_hello(self):#all function names should start with "test" for testing functions
+#         self.browser.get('http://127.0.0.1:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
 
 
+#     def tearDown(self):#after all Test are done
+#         self.browser.quit()
+
+class unitTestCase(TestCase):
+
+    def test_home_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')    
